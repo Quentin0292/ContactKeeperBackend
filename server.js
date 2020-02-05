42c5f38aa11e2dfd95ffaef7de85ba6d48e44e8b
Quentin <Quentin.lecocq@yahoo.com>
2020-02-05 11:57:00 +0100

    added backend routes & testing with Postman

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API' })
 );
 
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
 app.listen(PORT, () =>
   console.log(`Contact keeper app listening on port ${PORT}`)
 );
