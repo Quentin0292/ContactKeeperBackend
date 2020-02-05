bbfa1ddf46ebcb33961003b85da1a49c3cde81e8
Quentin <Quentin.lecocq@yahoo.com>
2020-02-05 12:54:05 +0100

    Connected MongoDB cloud to my app

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API' })
@@ -10,6 +14,8 @@ app.get('/', (req, res) =>
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(`Contact keeper app listening on port ${PORT}`)
