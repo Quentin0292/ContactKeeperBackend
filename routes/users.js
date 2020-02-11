1260757e37fb9bc03a2cb7a3dfdae11a0ace9373
Quentin <Quentin.lecocq@yahoo.com>
2020-02-11 20:38:37 +0100

    Added User Registration logic from the server to the database by the front

--- routes/users.js
+++ routes/users.js
@@ -16,45 +16,37 @@ router.post(
     check('name', 'Please add name')
       .not()
       .isEmpty(),
-    check('email', 'Please includ a valid email').isEmail(),
+    check('email', 'Please include a valid email').isEmail(),
     check(
       'password',
       'Please enter a password with 6 or more characters'
-    ).isLength({
-      min: 6
-    })
+    ).isLength({ min: 6 })
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(404).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
 
     try {
-      // try to find user in the DB
       let user = await User.findOne({ email });
 
-      // if user is true, it's mean user already exist, he can't register again
       if (user) {
         return res.status(400).json({ msg: 'User already exists' });
       }
 
-      // if doesn't exist create a new User
       user = new User({
         name,
         email,
         password
       });
 
-      // need to crypt the password before add the user in DB using bcryptjs
       const salt = await bcrypt.genSalt(10);
 
-      // update the value of password
       user.password = await bcrypt.hash(password, salt);
 
-      // saved user in db
       await user.save();
 
       const payload = {
@@ -76,7 +68,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(404).send('Server Error');
+      res.status(500).send('Server Error');
     }
   }
 );
